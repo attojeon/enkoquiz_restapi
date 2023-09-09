d28c369f6e5d7c7345b96f4eb7d31d497abc463b
attojeon <atto.jeon@gmail.com>
2023-09-09 17:43:11 +0900

    first upload

---
+++ app/app.py
@@ -0,0 +1,62 @@
+# flask 서비스 기본 코드
+
+from flask import Flask, render_template, request, redirect, url_for, session, jsonify
+import json, random 
+
+app = Flask("__english_dictionary__")
+
+# enko 딕셔너리 
+enko_dict = {}
+with open('data/english_korean_dictionary.json', 'r', encoding='utf-8') as f:
+    enko_dict = json.load(f)
+
+# enko 리스트
+enko_list = []
+for k, v in enko_dict.items():
+    enko_list.append((k, v))
+
+# enko quiz card 리스트 
+'''
+card = {
+    'en': 'apple',
+    'ko': '사과',
+    'options': ['사과', '바나나', '포도', '수박'] 
+    'result': 's' # s: success, f: fail , n: not yet
+}
+'''
+def build_quiz_card(word):
+    card = {}
+    card['en'] = word[0]
+    card['ko'] = word[1]
+    options = []
+    while len(options) < 3:
+        options.append(random.choice(enko_list)[1])
+        if card['ko'] in options:
+            options.remove(card['ko'])
+        options = list(set(options))
+    options.append(card['ko'])
+    random.shuffle(options)
+    card['options'] = options
+    card['result'] = 'n'
+    return card
+
+
+@app.route('/')
+def index():
+    return 'English Dictionary'
+
+@app.route('/ping')
+def ping():
+    return 'pong'
+
+@app.route('/quiz/<count>')
+def quiz(count):
+    quiz_list = []
+    quiz_ko_list = random.sample(enko_dict.items(), int(count))
+    for i in range(int(count)):
+        onecard = build_quiz_card(quiz_ko_list[i])
+        quiz_list.append(onecard)
+        print(onecard)
+    return jsonify(quiz_list)
+
+app.run(host="0.0.0.0", port="8080")
